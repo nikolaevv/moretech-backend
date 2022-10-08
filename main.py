57aa4cc24ae6fef88dc96b58895dc8c39e6cc63e
DanilKlochkov <danil.klochkov.92@bk.ru>
2022-10-09 00:32:00 +0300

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from internal.handlers.handlers import routes #, admin_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+) 
+
 app.include_router(routes)
 # app.include_router(admin_routes)
 
